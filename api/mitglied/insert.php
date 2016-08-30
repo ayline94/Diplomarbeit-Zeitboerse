496c095ede4c0fd9bacb27ae1e7d658ec6de508c
ayline94 <jana.heeb@gmail.com>
2016-08-30 06:25:15 +0200

    Sicherheit SQL

--- api/mitglied/insert.php
+++ api/mitglied/insert.php
@@ -2,7 +2,6 @@
 include ("../connect.php");
 
 // Felder prüfen
-
 if($_POST["vorname"] == '' || $_POST["nachname"] == '' || $_POST["email"] == '' || $_POST["passwort"] == '' || $_POST["geburtsdatum"] == '' || $_POST["strasse"] == '' || $_POST["plz"] == '' || $_POST["ort"] == '')
 {
     echo "Bitte alle Felder ausfüllen";
@@ -16,17 +15,23 @@ if($_POST["profilbild_pfad"] == '')
     return false;
 }
 
-// Felder definieren oder bearbeiten
-$email1 = $_POST["email"];
+// Felder schützen & bearbeiten
+$vorname = mysqli_real_escape_string($connect, $_POST["vorname"]);
+$nachname = mysqli_real_escape_string($connect, $_POST["nachname"]);
+$email = mysqli_real_escape_string($connect, $_POST["email"]);
 $passwort = md5(mysqli_real_escape_string($connect, $_POST["passwort"])); // Passwort verschlüsseln
-$profilbild = $_POST["profilbild_pfad"];
-
-$datum = new DateTime($_POST["geburtsdatum"]);
-$geburtsdatum = $datum->format('Y-m-d');
+$profilbild = mysqli_real_escape_string($connect, $_POST["profilbild_pfad"]);
+$datum = new DateTime(mysqli_real_escape_string($connect, $_POST["geburtsdatum"]));
+$geburtsdatum = $datum->format('Y-m-d'); //
+$strasse = mysqli_real_escape_string($connect, $_POST["strasse"]);
+$plz = mysqli_real_escape_string($connect, $_POST["plz"]);
+$ort = mysqli_real_escape_string($connect, $_POST["ort"]);
 
 
 // Felder in Datenbank schreiben
-$sql = "INSERT INTO mitglieder(vorname, nachname, email, passwort, profilbild_pfad, geburtsdatum, strasse, plz, ort) VALUES('".$_POST["vorname"]."', '".$_POST["nachname"]."', '".$email."', '".$passwort."', '".$profilbild."', '".$geburtsdatum."', '".$_POST["strasse"]."', '".$_POST["plz"]."', '".$_POST["ort"]."')";
+$sql = "INSERT
+        INTO mitglieder(vorname, nachname, email, passwort, profilbild_pfad, geburtsdatum, strasse, plz, ort)
+        VALUES('".$vorname."', '".$nachname."', '".$email."', '".$passwort."', '".$profilbild."', '".$geburtsdatum."', '".$strasse."', '".$plz."', '".$ort."')";
 
 // Rückmeldung Benutzer
 if(mysqli_query($connect, $sql))
